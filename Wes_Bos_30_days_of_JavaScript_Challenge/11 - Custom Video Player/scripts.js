13f942558622428b07d88e7d844dd6af0a84112a
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-08-11 18:13:27 -0700

    [add]skip buttons

--- Wes_Bos_30_days_of_JavaScript_Challenge/11 - Custom Video Player/scripts.js
+++ Wes_Bos_30_days_of_JavaScript_Challenge/11 - Custom Video Player/scripts.js
@@ -3,7 +3,7 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButton = player.querySelector('[data-skip]');
+const skipButton = player.querySelectorAll('[data-skip]');
 const range = player.querySelector('.player__slider');
 
 function togglePlay() {
@@ -13,7 +13,12 @@ function toggleButton() {
   toggle.textContent = video.paused ? 'play' : 'pause';
 }
 
+function handleSkip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', toggleButton);
 video.addEventListener('pause', toggleButton);
 toggle.addEventListener('click', togglePlay);
+skipButton.forEach(btn => btn.addEventListener('click', handleSkip));
