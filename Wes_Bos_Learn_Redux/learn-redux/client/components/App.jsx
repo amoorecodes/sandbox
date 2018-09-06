b55d51e585c887a1d0d131e45cc3969ebaec7f72
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-09-06 14:55:44 -0700

    [add] Router to the app

--- Wes_Bos_Learn_Redux/learn-redux/client/components/App.jsx
+++ Wes_Bos_Learn_Redux/learn-redux/client/components/App.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
+import PhotoGrid from './PhotoGrid.jsx';
 
-
-const App = React.createClass({
+class App extends Component {
   render() {
     return (
       <div>
         <h1>
           <Link to='/'>MooreStagram</Link>
         </h1>
+        {React.cloneElement(this.props.children, this.props)}
       </div>
     )
   }
-});
+}
 
 export default App;
--- Wes_Bos_Learn_Redux/learn-redux/client/reduxstagram.js
+++ Wes_Bos_Learn_Redux/learn-redux/client/reduxstagram.js
@@ -2,5 +2,19 @@ import React from 'react';
 import { render } from 'react-dom';
 import css from './styles/style.styl';
 import App from './components/App.jsx';
+import Photo from './components/Photo.jsx';
+import PhotoGrid from './components/PhotoGrid.jsx';
 
-render(<App/>, document.getElementById('root')); 
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Photo}></Route>
+    </Route>
+  </Router>
+  )
+
+
+render(router, document.getElementById('root')); 
