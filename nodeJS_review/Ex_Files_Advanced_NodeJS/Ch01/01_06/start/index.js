bc95614b471d3f4a9d2cfb8bafa416dd6d877e4b
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-09-08 18:45:31 -0700

    [add] Finish advanced node ex 0107

--- nodeJS_review/Ex_Files_Advanced_NodeJS/Ch01/01_06/start/index.js
+++ nodeJS_review/Ex_Files_Advanced_NodeJS/Ch01/01_06/start/index.js
@@ -1,19 +1,29 @@
 var fs = require('fs');
 var { promisify } = require('util');
 var writeFile = promisify(fs.writeFile);
+const readdir = promisify(fs.readdir);
 var unlink = promisify(fs.unlink);
 var beep = () => process.stdout.write("\x07");
 var delay = (seconds) => new Promise((resolves) => {
     setTimeout(resolves, seconds*1000);
-})
+});
+
+async function start() {
+  let files = await readdir(__dirname);
+  console.log(files);
+}
 
 const doStuffSequentially = async() => {
   console.log('starting');
   await delay(1);
   console.log('waiting');
   await delay(2);
-  await writeFile('someFile.txt', 'this is just the test..');
-  beep();
+  try {
+    await writeFile('someFile.txt', 'this is just the test..');
+    beep();
+  } catch(err) {
+    console.log('oops!');
+  }
   console.log('some file has been created');
   await delay(3);
   await unlink('someFile.txt');
@@ -21,4 +31,5 @@ const doStuffSequentially = async() => {
   console.log('some file has been deleted...');
 }
 
-doStuffSequentially()
+start();
+doStuffSequentially();
