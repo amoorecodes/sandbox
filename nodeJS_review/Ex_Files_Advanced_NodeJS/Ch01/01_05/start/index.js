c6921aa85cd4b74bd42982d4a59a7ee428798dc2
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-09-08 19:16:52 -0700

    [add] Fix commit

--- nodeJS_review/Ex_Files_Advanced_NodeJS/Ch01/01_05/start/index.js
+++ nodeJS_review/Ex_Files_Advanced_NodeJS/Ch01/01_05/start/index.js
@@ -1,38 +1,57 @@
 const fs = require('fs');
-
 const { promisify } = require('util');
 const writeFile = promisify(fs.writeFile);
 const unlink = promisify(fs.unlink); 
 const beep = () => process.stdout.write("\x07");
 
+const delay = (seconds) => new Promise((works) => {
+  setTimeout(works, seconds * 1000);
+});
 
-const doStuffSequentially = () => {
-    console.log('starting');
-    setTimeout(() => {
-        console.log('waiting');
-        setTimeout(() => {
-            console.log('waiting some more');
-            fs.writeFile('file.txt', 'Sample File...', error => {
-                if (error) {
-                    console.error(error);
-                } else {
-                    beep();
-                    console.log('file.txt created')
-                    setTimeout(() => {
-                        beep();
-                        fs.unlink('file.txt', error => {
-                            if (error) {
-                                console.error(error);
-                            } else {
-                                console.log('file.txt removed');
-                                console.log('sequential execution complete');
-                            }
-                        })
-                    }, 3000)
-                }
-            });
-        }, 2000)
-    }, 1000)
-}
+const sequentialExecution = () => Promise.resolve()
+  .then(() => console.log('starting'))
+  .then(() => delay(1))
+  .then((message) => console.log(message))
+  .then(() => delay(2))
+  .then(() => writeFile('file.txt', 'Just a test...'))
+  .then(beep)
+  .then(() => 'file.txt created')
+  .then(console.log)
+  .then(() => delay(3))
+  .then(() => unlink('file.txt'))
+  .then(beep)
+  .then(() => 'file.txt removed')
+  .then(console.log)
+  .catch((err) => console.log(err.message));
+
+
+// const doStuffSequentially = () => {
+//     console.log('starting');
+//     setTimeout(() => {
+//         console.log('waiting');
+//         setTimeout(() => {
+//             console.log('waiting some more');
+//             fs.writeFile('file.txt', 'Sample File...', error => {
+//                 if (error) {
+//                     console.error(error);
+//                 } else {
+//                     beep();
+//                     console.log('file.txt created')
+//                     setTimeout(() => {
+//                         beep();
+//                         fs.unlink('file.txt', error => {
+//                             if (error) {
+//                                 console.error(error);
+//                             } else {
+//                                 console.log('file.txt removed');
+//                                 console.log('sequential execution complete');
+//                             }
+//                         })
+//                     }, 3000)
+//                 }
+//             });
+//         }, 2000)
+//     }, 1000)
+// }
 
 doStuffSequentially();
