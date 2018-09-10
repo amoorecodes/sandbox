f220b468582ce2a23e46f6acd5f71de5dba66673
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-09-10 15:53:36 -0700

    [add] Finish advanced ex 0207

---
+++ nodeJS_review/Ex_Files_Advanced_NodeJS/Ch02/02_07/start/index.js
@@ -0,0 +1,43 @@
+const { Duplex, PassThrough } = require('stream');
+
+const { createReadStream, createWriteStream } = require('fs');
+
+const readStream = createReadStream('../../powder-day.mp4')
+const writeStream = createWriteStream('./copy.mp4');
+
+class Throttle extends Duplex {
+  constructor(ms) {
+    super();
+    this.delay = ms;
+
+  }
+
+  _write(chunk, encoding, callback) {
+    this.push(chunk);
+    setTimeout(callback, this.delay);
+  }
+
+  _read() {
+
+  }
+
+  _final() {
+    this.push(null);
+  }
+  
+}
+
+const report = new PassThrough();
+const throttle = new Throttle(10);
+
+let total = 0;
+
+report.on('data', (chunk) => {
+  total += chunk.length;
+  console.log(`${total} bytes went through.`);
+})
+
+readStream
+.pipe(throttle)
+  .pipe(report)
+  .pipe(writeStream);
